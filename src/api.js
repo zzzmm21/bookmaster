077aadcf7b675b824cd45e33a04ecbe298012da5
zzzmm21 <114974461+zzzmm21@users.noreply.github.com>
2023-01-27 11:35:33 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const Kakao = axios.create({
   baseURL: 'https://dapi.kakao.com', // 공통 요청 경로를 지정해준다.
   headers: {
-    Authorization: `KakaoAK a2ceaaa9b5d8805bcef9c6320a873f8c`,
+    Authorization: `KakaoAK 그러지마세요`,
   },
 });
 
